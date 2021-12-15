83f69955b33f84b0a5a30e545cb85f4cad5a7873
SHAMS AHMAD <75977026+cbisam69@users.noreply.github.com>
2021-12-16 01:40:26 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,x=0,y;// y is the number &x is count of number of times y is divisible
+    printf("enter the number\n");
+    scanf("%d",&y);
+for(i=1;i<=y;i++)
+{
+    x++;
+    
+}
+if(x==2)
+printf("its is a prime no");
+else
+printf("ITS NOT A Prime");
+}
